42aacad6e35c71ed5041bda12a2ed96089148b38
Yara-Abdallah <yaraaabdallaah@gmail.com>
2023-09-30 10:30:11 +0300

    0 , 10 , -1 , 100 ep

--- reward_plotting.py
+++ reward_plotting.py
@@ -3,7 +3,7 @@ import pickle
 import sys
 
 outlet_num = 0
-results_dir = os.path.join(sys.path[0],f"action_each_single_request_reward_method4_add_init_10_11_12_less_failure_less_reward_100_320_save//qvalue_decentralized_for_plotting//qvalue{outlet_num}.pkl")
+results_dir = os.path.join(sys.path[0],f"action_each_single_request_reward_method4_add_init_10_11_12_less_failure_0_10_m1//qvalue_decentralized_for_plotting//qvalue{outlet_num}.pkl")
 # results_dir2 = os.path.join(sys.path[0],f"action_each_single_request_reward_method4_add_init_10_11_12_no_protrization_less_failure_less_reward_2//reward_accumilated_decentralize//accu_reward{outlet_num}.pkl")
 
 
